0394993cfc76215fab3543aa2f5a27b664e692ba
heywb456789 <heywb457@naver.com>
2024-05-22 10:03:17 +0900

    first Commit

---
+++ lectures/1_first_app/main.js
@@ -0,0 +1,17 @@
+let number = 'Hello Javascript';
+
+function sayHello(){
+  console.log("hello");
+  console.log("hi")
+}
+
+sayHello();
+
+let obj = {
+  number : 1,
+  sayhello : function(){
+    console.log(hi);
+  }
+}
+
+obj.sayhello();
